1d74ddd45c3842dbf44208beefc2a3abe2ee2e39
Nikolay Nikolaev <evrinoma@gmail.com>
2021-09-07 19:55:08 +0300

    controller inject Manager and DTO class identity

--- src/Controller/CodeApiController.php
+++ src/Controller/CodeApiController.php
@@ -2,8 +2,11 @@
 
 namespace Evrinoma\CodeBundle\Controller;
 
+use Evrinoma\CodeBundle\Manager\CommandManagerInterface;
+use Evrinoma\ContractorBundle\Manager\QueryManagerInterface;
 use Evrinoma\DtoBundle\Factory\FactoryDtoInterface;
 use Evrinoma\UtilsBundle\Controller\AbstractApiController;
+use Evrinoma\UtilsBundle\Rest\RestInterface;
 use JMS\Serializer\SerializerInterface;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\RequestStack;
@@ -15,27 +18,49 @@ final class CodeApiController extends AbstractApiController
      * @var ?Request
      */
     private ?Request $request;
+    /**
+     * @var FactoryDtoInterface
+     */
+    private FactoryDtoInterface $factoryDto;
+    /**
+     * @var CommandManagerInterface|RestInterface
+     */
+    private CommandManagerInterface $commandManager;
+    /**
+     * @var QueryManagerInterface|RestInterface
+     */
+    private QueryManagerInterface $queryManager;
+    /**
+     * @var string
+     */
+    private string $dtoClass;
 //endregion Fields
 
 //region SECTION: Constructor
-
-
     /**
      * ApiController constructor.
      *
-     * @param SerializerInterface $serializer
-     * @param RequestStack        $requestStack
-     * @param FactoryDtoInterface $factoryDto
+     * @param SerializerInterface     $serializer
+     * @param RequestStack            $requestStack
+     * @param FactoryDtoInterface     $factoryDto
+     * @param CommandManagerInterface $commandManager
+     * @param QueryManagerInterface   $queryManager
+     * @param string                  $dtoClass
      */
     public function __construct(
         SerializerInterface $serializer,
         RequestStack $requestStack,
-        FactoryDtoInterface $factoryDto
+        FactoryDtoInterface $factoryDto,
+        CommandManagerInterface $commandManager,
+        QueryManagerInterface $queryManager,
+        string $dtoClass
     ) {
         parent::__construct($serializer);
-        $this->request    = $requestStack->getCurrentRequest();
-        $this->factoryDto = $factoryDto;
+        $this->request        = $requestStack->getCurrentRequest();
+        $this->factoryDto     = $factoryDto;
+        $this->commandManager = $commandManager;
+        $this->queryManager   = $queryManager;
+        $this->dtoClass       = $dtoClass;
     }
-
 //endregion Constructor
 }
