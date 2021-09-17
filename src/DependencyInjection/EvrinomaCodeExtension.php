1778cea445c61b79af044cda58c4fb43553da88e
Nikolay Nikolaev <evrinoma@gmail.com>
2021-09-17 16:53:31 +0300

    fix configuration

--- src/DependencyInjection/EvrinomaCodeExtension.php
+++ src/DependencyInjection/EvrinomaCodeExtension.php
@@ -3,20 +3,37 @@
 
 namespace Evrinoma\CodeBundle\DependencyInjection;
 
-use Evrinoma\CodeBundle\EvrinomaCodeBundle;
 use Evrinoma\CodeBundle\Dto\CodeApiDto;
+use Evrinoma\CodeBundle\EvrinomaCodeBundle;
+use Evrinoma\UtilsBundle\DependencyInjection\HelperTrait;
 use Symfony\Component\Config\FileLocator;
 use Symfony\Component\DependencyInjection\Alias;
 use Symfony\Component\DependencyInjection\ContainerBuilder;
 use Symfony\Component\DependencyInjection\Definition;
 use Symfony\Component\DependencyInjection\Extension\Extension;
 use Symfony\Component\DependencyInjection\Loader\YamlFileLoader;
+use Symfony\Component\DependencyInjection\Reference;
 
 class EvrinomaCodeExtension extends Extension
 {
-    public const ENTITY_FACTORY           = 'Evrinoma\CodeBundle\Factory\CodeFactory';
-    public const ENTITY_BASE_CODE         = 'Evrinoma\CodeBundle\Entity\Basic\BaseCode';
-    public const ENTITY_BASE_BUNCH        = 'Evrinoma\CodeBundle\Entity\Basic\BaseBunch';
+    use HelperTrait;
+
+//region SECTION: Fields
+    public const ENTITY            = 'Evrinoma\CodeBundle\Entity';
+    public const ENTITY_FACTORY    = 'Evrinoma\CodeBundle\Factory\CodeFactory';
+    public const ENTITY_BASE_CODE  = self::ENTITY.'\Basic\BaseCode';
+    public const ENTITY_BASE_BUNCH = self::ENTITY.'\Basic\BaseBunch';
+
+    /**
+     * @var array
+     */
+    private static array $doctrineDrivers = array(
+        'orm' => array(
+            'registry' => 'doctrine',
+            'tag'      => 'doctrine.event_subscriber',
+        ),
+    );
+//endregion Fields
 
 //region SECTION: Public
     public function load(array $configs, ContainerBuilder $container)
@@ -32,11 +49,33 @@ class EvrinomaCodeExtension extends Extension
         if ($config['factory'] !== self::ENTITY_FACTORY) {
             $container->removeDefinition('evrinoma.'.$this->getAlias().'.factory');
             $definitionFactory = new Definition($config['factory']);
-            $alias = new Alias('evrinoma.'.$this->getAlias().'.factory');
-            $container->addDefinitions([ 'evrinoma.'.$this->getAlias().'.factory' => $definitionFactory]);
+            $alias             = new Alias('evrinoma.'.$this->getAlias().'.factory');
+            $container->addDefinitions(['evrinoma.'.$this->getAlias().'.factory' => $definitionFactory]);
             $container->addAliases([$config['factory'] => $alias]);
         }
 
+        if (isset(self::$doctrineDrivers[$config['db_driver']])) {
+            $loader->load('doctrine.yml');
+            $container->setAlias('evrinoma.'.$this->getAlias().'.doctrine_registry', new Alias(self::$doctrineDrivers[$config['db_driver']]['registry'], false));
+
+
+            $container->setParameter('evrinoma.'.$this->getAlias().'.backend_type_'.$config['db_driver'], true);
+
+            $definitionRepository = $container->getDefinition('evrinoma.'.$this->getAlias().'.object_manager');
+            $definitionRepository->setFactory([new Reference('evrinoma.'.$this->getAlias().'.doctrine_registry'), 'getManager']);
+        }
+
+        $this->remapParametersNamespaces(
+            $container,
+            $config,
+            [
+                '' => [
+                    'db_driver'    => 'evrinoma.'.$this->getAlias().'.storage',
+                    'entity_bunch' => 'evrinoma.'.$this->getAlias().'.entity_bunch',
+                    'entity_code'  => 'evrinoma.'.$this->getAlias().'.entity_code',
+                ],
+            ]
+        );
 
     }
 //endregion Public
