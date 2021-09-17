66f604662ba611d72bd87bd30ad39f772e7130a7
Nikolay Nikolaev <evrinoma@gmail.com>
2021-09-17 16:53:17 +0300

    enable compiler pass

--- src/EvrinomaCodeBundle.php
+++ src/EvrinomaCodeBundle.php
@@ -3,7 +3,9 @@
 
 namespace Evrinoma\CodeBundle;
 
+use Evrinoma\CodeBundle\DependencyInjection\Compiler\MapEntityPass;
 use Evrinoma\CodeBundle\DependencyInjection\EvrinomaCodeExtension;
+use Symfony\Component\DependencyInjection\ContainerBuilder;
 use Symfony\Component\HttpKernel\Bundle\Bundle;
 
 
@@ -16,6 +18,14 @@ class EvrinomaCodeBundle extends Bundle
 {
 //region SECTION: Fields
     public const CODE_BUNDLE = 'code';
+
+    public function build(ContainerBuilder $container)
+    {
+        parent::build($container);
+        $container
+            ->addCompilerPass(new MapEntityPass($this->getNamespace(), $this->getPath()))
+        ;
+    }
 //endregion Fields
 
 //region SECTION: Getters/Setters
