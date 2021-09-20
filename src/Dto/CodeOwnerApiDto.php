1818a8d353da61aaf04f85ed5c57d0df0d4bd144
Nikolay Nikolaev <evrinoma@gmail.com>
2021-09-20 16:04:09 +0300

    Dto describer fix

--- src/Dto/CodeOwnerApiDto.php
+++ src/Dto/CodeOwnerApiDto.php
@@ -6,11 +6,16 @@ use Evrinoma\DtoBundle\Dto\AbstractDto;
 use Evrinoma\DtoBundle\Dto\DtoInterface;
 use Symfony\Component\HttpFoundation\Request;
 
-final class CodeApiDto extends AbstractDto implements CodeApiDtoInterface
+final class CodeOwnerApiDto extends AbstractDto implements CodeOwnerApiDtoInterface
 {
 
     public function toDto(Request $request): DtoInterface
     {
         return $this;
+    }
+
+    public function hasId(): bool
+    {
+        // TODO: Implement hasId() method.
     }
 }
---
+++ src/Dto/CodeOwnerApiDtoInterface.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace Evrinoma\CodeBundle\Dto;
+
+interface CodeOwnerApiDtoInterface
+{
+    /**
+     * @return bool
+     */
+    public function hasId(): bool;
+}
