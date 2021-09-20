441101c85be0e009ada04524d04e78ae04e9b988
Nikolay Nikolaev <evrinoma@gmail.com>
2021-09-20 13:59:55 +0300

    split logic into new abstract layer

--- src/DependencyInjection/Compiler/MapEntityPass.php
+++ src/DependencyInjection/Compiler/MapEntityPass.php
@@ -2,72 +2,48 @@
 
 namespace Evrinoma\CodeBundle\DependencyInjection\Compiler;
 
-use Doctrine\ORM\Mapping\Driver\AnnotationDriver;
 use Evrinoma\CodeBundle\DependencyInjection\EvrinomaCodeExtension;
-use Evrinoma\ContractorBundle\DependencyInjection\EvrinomaContractorExtension;
+use Evrinoma\CodeBundle\Entity\BaseBunch;
+use Evrinoma\CodeBundle\Entity\BaseOwner;
+use Evrinoma\CodeBundle\Entity\BaseType;
+use Evrinoma\CodeBundle\Model\BunchInterface;
+use Evrinoma\CodeBundle\Model\OwnerInterface;
+use Evrinoma\CodeBundle\Model\TypeInterface;
+use Evrinoma\UtilsBundle\DependencyInjection\Compiler\MapEntityAbstract;
 use Symfony\Component\DependencyInjection\Compiler\CompilerPassInterface;
 use Symfony\Component\DependencyInjection\ContainerBuilder;
-use Symfony\Component\DependencyInjection\Definition;
 use Symfony\Component\DependencyInjection\Reference;
 
-class MapEntityPass implements CompilerPassInterface
+class MapEntityPass extends MapEntityAbstract implements CompilerPassInterface
 {
-//region SECTION: Fields
-    private string $nameSpace;
-    private string $path;
-//endregion Fields
-//region SECTION: Constructor
-    /**
-     * MapEntityPass constructor.
-     *
-     * @param string $nameSpace
-     */
-    public function __construct(string $nameSpace, string $path)
-    {
-        $this->nameSpace = $nameSpace;
-        $this->path      = $path;
-    }
-//endregion Constructor
-
 //region SECTION: Public
     /**
      * @inheritDoc
      */
     public function process(ContainerBuilder $container)
     {
+        $this->setContainer($container);
+
         $driver                    = $container->findDefinition('doctrine.orm.default_metadata_driver');
         $referenceAnnotationReader = new Reference('annotations.reader');
-
 
         $entityBunch = $container->getParameter('evrinoma.code.entity_code');
         $entityCode  = $container->getParameter('evrinoma.code.entity_bunch');
         if ((strpos($entityBunch, EvrinomaCodeExtension::ENTITY) !== false) && (strpos($entityCode, EvrinomaCodeExtension::ENTITY) !== false)) {
             $this->loadMetadata($driver, $referenceAnnotationReader, '%s/Model', '%s/Entity');
+            $this->addResolveTargetEntity(
+                [
+                    BaseBunch::class => BunchInterface::class,
+                    BaseOwner::class => OwnerInterface::class,
+                    BaseType::class  => TypeInterface::class,
+                ],
+                false
+            );
+
         } else {
             $this->cleanMetadata($driver, [EvrinomaCodeExtension::ENTITY]);
         }
     }
 
-//region SECTION: Private
-    private function loadMetadata(Definition $driver, Reference $referenceAnnotationReader, $formatterModel, $formatterEntity): void
-    {
-        $definitionAnnotationDriver = new Definition(AnnotationDriver::class, [$referenceAnnotationReader, sprintf($formatterModel, $this->path)]);
-        $driver->addMethodCall('addDriver', [$definitionAnnotationDriver, sprintf(str_replace('/', '\\', $formatterModel), $this->nameSpace)]);
-
-        $definitionAnnotationDriver = new Definition(AnnotationDriver::class, [$referenceAnnotationReader, sprintf($formatterEntity, $this->path)]);
-        $driver->addMethodCall('addDriver', [$definitionAnnotationDriver, sprintf(str_replace('/', '\\', $formatterEntity), $this->nameSpace)]);
-    }
-
-    private function cleanMetadata(Definition $driver, array $namesSpaces)
-    {
-        $calls = [];
-        foreach ($driver->getMethodCalls() as $i => $call) {
-            if ($call[1][1] && in_array($call[1][1], $namesSpaces)) {
-                continue;
-            }
-            $calls[] = $call;
-        }
-        $driver->setMethodCalls($calls);
-    }
 //endregion Private
 }
