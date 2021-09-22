fcc289b9f465b5fe9a6b6fc0ac77fd300b05e30d
Nikolay Nikolaev <evrinoma@gmail.com>
2021-09-22 18:35:09 +0300

    Fix dto bunch

--- src/Dto/BunchApiDto.php
+++ src/Dto/BunchApiDto.php
@@ -8,9 +8,63 @@ use Symfony\Component\HttpFoundation\Request;
 
 class BunchApiDto extends AbstractDto implements BunchApiDtoInterface
 {
+    /**
+     * @Dto(class="Evrinoma\CodeBundle\Dto\TypeApiDto", generator="genRequestTypeApiDto")
+     * @var TypeApiDto
+     */
+    private $typeApiDto;
 
     public function toDto(Request $request): DtoInterface
     {
         return $this;
+    }
+
+    public function hasId(): bool
+    {
+        // TODO: Implement hasId() method.
+    }
+
+    public function getId(): string
+    {
+        // TODO: Implement getId() method.
+    }
+
+    /**
+     * @return TypeApiDto
+     */
+    public function getTypeApiDto(): TypeApiDto
+    {
+        return $this->typeApiDto;
+    }
+
+    /**
+     * @param TypeApiDto $typeApiDto
+     */
+    private function setTypeApiDto(TypeApiDto $typeApiDto): void
+    {
+        $this->typeApiDto = $typeApiDto;
+    }
+
+
+    public function getDescription(): string
+    {
+        // TODO: Implement getDescription() method.
+    }
+
+    /**
+     * @return \Generator
+     */
+    public function genRequestTypeApiDto(?Request $request): ?\Generator
+    {
+        if ($request) {
+            $domain = $request->get('type');
+            if ($domain) {
+                $newRequest                      = $this->getCloneRequest();
+                $domain[DtoInterface::DTO_CLASS] = TypeApiDto::class;
+                $newRequest->request->add($domain);
+
+                yield $newRequest;
+            }
+        }
     }
 }
--- src/Dto/BunchApiDtoInterface.php
+++ src/Dto/BunchApiDtoInterface.php
@@ -7,4 +7,17 @@ interface BunchApiDtoInterface
 //region SECTION: Public
     public function hasId(): bool;
 //endregion Public
+
+//region SECTION: Dto
+    public function getTypeApiDto(): TypeApiDto;
+//endregion SECTION: Dto
+
+//region SECTION: Getters/Setters
+    public function getId(): string;
+
+    /**
+     * @return string
+     */
+    public function getDescription(): string;
+//endregion Getters/Setters
 }
