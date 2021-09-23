e618827a7343aaa6f0c099cf8f3e3e49167c1fc0
Nikolay Nikolaev <evrinoma@gmail.com>
2021-09-23 10:43:32 +0300

    init repository bunch

---
+++ src/Repository/Bunch/BunchQueryRepositoryInterface.php
@@ -0,0 +1,8 @@
+<?php
+
+namespace Evrinoma\CodeBundle\Repository\Bunch;
+
+interface BunchQueryRepositoryInterface
+{
+
+}
---
+++ src/Repository/Bunch/BunchRepositoryInterface.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace Evrinoma\CodeBundle\Repository\Bunch;
+
+
+interface BunchRepositoryInterface extends BunchQueryRepositoryInterface,BunchCommandRepositoryInterface
+{
+
+}
