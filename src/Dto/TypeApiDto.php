91fc78ce618b27f05f4470d37ed7e22ccf0764b8
Nikolay Nikolaev <evrinoma@gmail.com>
2021-09-29 18:30:49 +0300

    fix Type Dto

--- src/Dto/TypeApiDto.php
+++ src/Dto/TypeApiDto.php
@@ -2,21 +2,20 @@
 
 namespace Evrinoma\CodeBundle\Dto;
 
+use Evrinoma\CodeBundle\Model\ModelInterface;
 use Evrinoma\DtoBundle\Dto\AbstractDto;
 use Evrinoma\DtoBundle\Dto\DtoInterface;
 use Symfony\Component\HttpFoundation\Request;
 
 final class TypeApiDto extends AbstractDto implements TypeApiDtoInterface
 {
+//region SECTION: Fields
     private string $id = '';
 
     private string $brief = '';
+//endregion Fields
 
-    public function toDto(Request $request): DtoInterface
-    {
-        return $this;
-    }
-
+//region SECTION: Public
     /**
      * @return bool
      */
@@ -32,7 +31,58 @@ final class TypeApiDto extends AbstractDto implements TypeApiDtoInterface
     {
         return $this->brief !== '';
     }
+//endregion Public
 
+//region SECTION: Private
+    /**
+     * @param string $id
+     *
+     * @return TypeApiDto
+     */
+    private function setId(string $id): TypeApiDto
+    {
+        $this->id = $id;
+
+        return $this;
+    }
+
+    /**
+     * @param string $brief
+     *
+     * @return TypeApiDto
+     */
+    private function setBrief(string $brief): TypeApiDto
+    {
+        $this->brief = $brief;
+
+        return $this;
+    }
+//endregion Private
+
+//region SECTION: Dto
+    public function toDto(Request $request): DtoInterface
+    {
+        $class = $request->get(DtoInterface::DTO_CLASS);
+
+        if ($class === $this->getClass()) {
+            $id    = $request->get(ModelInterface::ID);
+            $brief = $request->get(ModelInterface::BRIEF);
+
+            if ($brief) {
+                $this->setBrief($brief);
+            }
+
+
+            if ($id) {
+                $this->setId($id);
+            }
+        }
+
+        return $this;
+    }
+//endregion SECTION: Dto
+
+//region SECTION: Getters/Setters
     /**
      * @return string
      */
@@ -48,4 +98,5 @@ final class TypeApiDto extends AbstractDto implements TypeApiDtoInterface
     {
         return $this->brief;
     }
+//endregion Getters/Setters
 }
