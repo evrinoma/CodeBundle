25a1a04ec14070c5e1d0d625a01f74fb44bb43d9
Nikolay Nikolaev <evrinoma@gmail.com>
2021-09-30 23:37:25 +0300

    fix parameters

--- src/DependencyInjection/Compiler/DecoratorPass.php
+++ src/DependencyInjection/Compiler/DecoratorPass.php
@@ -15,26 +15,26 @@ class DecoratorPass extends AbstractRecursivePass
      */
     public function process(ContainerBuilder $container)
     {
-        $decoratorQuery = $container->getParameter('evrinoma.contractor.decorates.code.query');
+        $decoratorQuery = $container->getParameter('evrinoma.'.EvrinomaCodeBundle::CODE_BUNDLE.'.decorates.code.query');
         if ($decoratorQuery) {
             $queryMediator = $container->getDefinition($decoratorQuery);
             $repository    = $container->getDefinition('evrinoma.'.EvrinomaCodeBundle::CODE_BUNDLE.'.code.repository');
             $repository->setArgument(2, $queryMediator);
         }
-        $decoratorCommand = $container->getParameter('evrinoma.contractor.decorates.code.command');
+        $decoratorCommand = $container->getParameter('evrinoma.'.EvrinomaCodeBundle::CODE_BUNDLE.'.decorates.code.command');
         if ($decoratorCommand) {
             $commandMediator = $container->getDefinition($decoratorCommand);
             $commandManager  = $container->getDefinition('evrinoma.'.EvrinomaCodeBundle::CODE_BUNDLE.'.code.command.manager');
             $commandManager->setArgument(3, $commandMediator);
         }
 
-        $decoratorQuery = $container->getParameter('evrinoma.contractor.decorates.bunch.query');
+        $decoratorQuery = $container->getParameter('evrinoma.'.EvrinomaCodeBundle::CODE_BUNDLE.'.decorates.bunch.query');
         if ($decoratorQuery) {
             $queryMediator = $container->getDefinition($decoratorQuery);
             $repository    = $container->getDefinition('evrinoma.'.EvrinomaCodeBundle::CODE_BUNDLE.'.bunch.repository');
             $repository->setArgument(2, $queryMediator);
         }
-        $decoratorCommand = $container->getParameter('evrinoma.contractor.decorates.bunch.command');
+        $decoratorCommand = $container->getParameter('evrinoma.'.EvrinomaCodeBundle::CODE_BUNDLE.'.decorates.bunch.command');
         if ($decoratorCommand) {
             $commandMediator = $container->getDefinition($decoratorCommand);
             $commandManager  = $container->getDefinition('evrinoma.'.EvrinomaCodeBundle::CODE_BUNDLE.'.bunch.command.manager');
