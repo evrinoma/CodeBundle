8eb5f2b8469e96c50939a40d6c2a005363401d55
Nikolay Nikolaev <evrinoma@gmail.com>
2021-10-02 22:43:30 +0300

    fixtures

---
+++ src/Fixtures/FixtureInterface.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace Evrinoma\CodeBundle\Fixtures;
+
+interface FixtureInterface
+{
+//region SECTION: Fields
+    public const TYPE_FIXTURES  = 'type';
+    public const OWNER_FIXTURES = 'owner';
+    public const CODE_FIXTURES  = 'code';
+//endregion Fields
+}
