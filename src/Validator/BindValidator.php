dda77453b6658705d345cba49c98c623796bffff
Nikolay Nikolaev <evrinoma@gmail.com>
2021-10-02 20:32:09 +0300

    Bind Entity validator

---
+++ src/Validator/BindValidator.php
@@ -0,0 +1,30 @@
+<?php
+
+
+namespace Evrinoma\CodeBundle\Validator;
+
+
+use Evrinoma\CodeBundle\Entity\Bunch\BaseBind;
+use Evrinoma\UtilsBundle\Validator\AbstractValidator;
+
+final class BindValidator extends AbstractValidator
+{
+//region SECTION: Fields
+    /**
+     * @var string|null
+     */
+    protected static ?string $entityClass = BaseBind::class;
+//endregion Fields
+
+//region SECTION: Constructor
+    /**
+     * ContractorValidator constructor.
+     *
+     * @param string $entityClass
+     */
+    public function __construct(string $entityClass)
+    {
+        parent::__construct($entityClass);
+    }
+//endregion Constructor
+}
