4288f57504b15b674b4906e5327fe5f5ec9f1ece
Nikolay Nikolaev <evrinoma@gmail.com>
2021-10-03 19:13:37 +0300

    test bind

--- src/Tests/Functional/Controller/BindApiControllerTest.php
+++ src/Tests/Functional/Controller/BindApiControllerTest.php
@@ -4,6 +4,7 @@ namespace Evrinoma\CodeBundle\Tests\Functional\Controller;
 
 
 use Evrinoma\CodeBundle\Dto\BindApiDto;
+use Evrinoma\CodeBundle\Fixtures\FixtureInterface;
 use Evrinoma\CodeBundle\Tests\Functional\CaseTest;
 use Evrinoma\TestUtilsBundle\Browser\ApiBrowserTestInterface;
 use Evrinoma\TestUtilsBundle\Browser\ApiBrowserTestTrait;
@@ -34,186 +35,158 @@ class BindApiControllerTest extends CaseTest implements ApiControllerTestInterfa
         return BindApiDto::class;
     }
 
+    protected function getFixtures(): array
+    {
+        return [FixtureInterface::BIND_FIXTURES];
+    }
+
     protected static function defaultData(): array
     {
-//        return [
-//            "id"          => 1,
-//            "type"        => TypeApiControllerTest::defaultData(),
-//            "description" => "desc",
-//            "class"       => static::getDtoClass(),
-//        ];
-        return [];
+        return [
+            "id"     => 9,
+            "bunch"  => BunchApiControllerTest::defaultData(),
+            "code"   => CodeApiControllerTest::defaultData(),
+            "active" => "a",
+            "class"  => static::getDtoClass(),
+        ];
     }
 
 //endregion Protected
 //region SECTION: Public
-    public function testPostUnprocessable(): void
+
+    public function testCriteria(): void
     {
-        $this->postWrong();
-        $this->assertEquals(Response::HTTP_UNPROCESSABLE_ENTITY, $this->client->getResponse()->getStatusCode());
-
-        $this->createConstraintBlankType();
-        $this->assertEquals(Response::HTTP_UNPROCESSABLE_ENTITY, $this->client->getResponse()->getStatusCode());
-
-        $this->createConstraintBlankDescription();
-        $this->assertEquals(Response::HTTP_UNPROCESSABLE_ENTITY, $this->client->getResponse()->getStatusCode());
-    }
-
-    public function testGet(): void
-    {
-        $type = $this->createType();
-        $this->assertArrayHasKey('data', $type);
-
-        $created = $this->createBunch();
-        $this->assertEquals(Response::HTTP_CREATED, $this->client->getResponse()->getStatusCode());
-
-        $find = $this->get(1);
+        $find = $this->criteria(["class" => static::getDtoClass(), "active" => "a", "code" => ["brief" => 'KD', "description" => 'sdf'], "bunch" => ["description" => 'ca'] ]);
         $this->assertEquals(Response::HTTP_OK, $this->client->getResponse()->getStatusCode());
+        $this->assertCount(1, $find['data']);
 
-        $this->assertArrayHasKey('data', $created);
-        $this->assertArrayHasKey('data', $find);
-
-        $this->assertArrayHasKey('type', $created['data']);
-        $this->assertArrayHasKey('type', $find['data']);
-
-        $this->assertStringContainsString(serialize($created['data']), serialize($find['data']));
-    }
-
-    public function testDelete(): void
-    {
-        $type = $this->createType();
-        $this->assertArrayHasKey('data', $type);
-
-        $created = $this->createBunch();
-        $this->assertEquals(Response::HTTP_CREATED, $this->client->getResponse()->getStatusCode());
-
-        $find = $this->get(1);
+        $find = $this->criteria(["class" => static::getDtoClass(), "active" => "d"]);
         $this->assertEquals(Response::HTTP_OK, $this->client->getResponse()->getStatusCode());
+        $this->assertCount(3, $find['data']);
 
-        $this->assertArrayHasKey('data', $created);
-        $this->assertArrayHasKey('data', $find);
-        $this->assertArrayHasKey('active', $find['data']);
-
-        $this->delete('1');
-        $this->assertEquals(Response::HTTP_ACCEPTED, $this->client->getResponse()->getStatusCode());
-
-        $delete = $this->get(1);
+        $find = $this->criteria(["class" => static::getDtoClass(), "active" => "a", "bunch" => ["description" => 'ca'] ]);
         $this->assertEquals(Response::HTTP_OK, $this->client->getResponse()->getStatusCode());
+        $this->assertCount(4, $find['data']);
 
-        $this->assertArrayHasKey('data', $delete);
-        $this->assertArrayHasKey('active', $delete['data']);
+        $find = $this->criteria(["class" => static::getDtoClass(), "active" => "a", "code" => ["description" => 'asd'] ]);
+        $this->assertEquals(Response::HTTP_OK, $this->client->getResponse()->getStatusCode());
+        $this->assertCount(2, $find['data']);
 
-        $this->assertEquals(ActiveModel::DELETED, $delete['data']['active']);
-    }
+        $find = $this->criteria(["class" => static::getDtoClass(), "active" => "a", "code" => ["brief" => 'KD'] ]);
+        $this->assertEquals(Response::HTTP_OK, $this->client->getResponse()->getStatusCode());
+        $this->assertCount(2, $find['data']);
 
-    public function testPutUnprocessable(): void
-    {
-        $type = $this->createType();
-        $this->assertArrayHasKey('data', $type);
-
-        $query = [
-            "class"       => static::getDtoClass(),
-            "id"          => "",
-            "description" => "0987654321",
-            "type"        => TypeApiControllerTest::defaultData(),
-        ];
-
-        $this->put($query);
-        $this->assertEquals(Response::HTTP_UNPROCESSABLE_ENTITY, $this->client->getResponse()->getStatusCode());
-
-        $this->createBunch();
-        $this->assertEquals(Response::HTTP_CREATED, $this->client->getResponse()->getStatusCode());
-
-        $query = [
-            "id"          => "1",
-            "description" => "0987654321",
-            "type"        => TypeApiControllerTest::defaultData(),
-        ];
-
-        $this->put($query);
-        $this->assertEquals(Response::HTTP_UNPROCESSABLE_ENTITY, $this->client->getResponse()->getStatusCode());
-
-        $query = [
-            "id"          => "1",
-            "description" => "0987654321",
-        ];
-
-        $this->put($query);
-        $this->assertEquals(Response::HTTP_UNPROCESSABLE_ENTITY, $this->client->getResponse()->getStatusCode());
+        $find = $this->criteria(["class" => static::getDtoClass(), "active" => "a", "code" => ["brief" => 'KD'], "bunch" => ["description" => 'ca'] ]);
+        $this->assertEquals(Response::HTTP_OK, $this->client->getResponse()->getStatusCode());
+        $this->assertCount(1, $find['data']);
     }
 
     public function testCriteriaNotFound(): void
     {
-        $type = $this->createType();
-        $this->assertArrayHasKey('data', $type);
-
-        $this->createBunch();
-        $this->assertEquals(Response::HTTP_CREATED, $this->client->getResponse()->getStatusCode());
-        $this->createBunchSecond();
-        $this->assertEquals(Response::HTTP_CREATED, $this->client->getResponse()->getStatusCode());
-
-        $response = $this->criteria(["class" => static::getDtoClass(), "description" => "ddes"]);
+        $find = $this->criteria(["class" => static::getDtoClass(), "active" => "b"]);
         $this->assertEquals(Response::HTTP_NOT_FOUND, $this->client->getResponse()->getStatusCode());
-        $this->assertArrayHasKey('data', $response);
+        $this->assertArrayHasKey('data', $find);
+
+        $find = $this->criteria(["class" => static::getDtoClass(), "active" => "a", "code" => ["brief" => 'KD', "description" => 'sdf'], "bunch" => ["description" => 'v'] ]);
+        $this->assertEquals(Response::HTTP_NOT_FOUND, $this->client->getResponse()->getStatusCode());
+        $this->assertArrayHasKey('data', $find);
     }
 
     public function testPut(): void
     {
-        $type = $this->createType();
-        $this->assertArrayHasKey('data', $type);
+        $find = $this->assertGet(6);
 
-        $created = $this->createBunch();
-        $this->assertEquals(Response::HTTP_CREATED, $this->client->getResponse()->getStatusCode());
-
-        $find = $this->get(1);
+        $updated = $this->put($this->getDefault(['id' => 6, 'code' => ['id' => 4]]));
         $this->assertEquals(Response::HTTP_OK, $this->client->getResponse()->getStatusCode());
 
-        $this->assertArrayHasKey('data', $created);
-        $this->assertArrayHasKey('data', $find);
+        $this->assertNotEquals($find['data']['code']['id'], $updated['data']['code']['id']);
+        $this->assertEquals(4, $updated['data']['code']['id']);
 
-        $this->assertArrayHasKey('type', $created['data']);
-        $this->assertArrayHasKey('type', $find['data']);
-
-        $this->assertCount(0, array_diff($created['data']['type'], $find['data']['type']));
-
-        $query = [
-            "class"       => static::getDtoClass(),
-            "id"          => $find['data']['id'],
-            "description" => "noitpircsed",
-            "type"        => TypeApiControllerTest::defaultData(),
-        ];
-
-        $saved = $this->put($query);
-        $this->assertEquals(Response::HTTP_OK, $this->client->getResponse()->getStatusCode());
-        $this->assertArrayHasKey('data', $saved);
-        $this->assertArrayHasKey('updated_at', $saved['data']);
+        $findUpdated = $this->assertGet(6);
+        $this->assertStringContainsString(serialize($updated['data']), serialize($findUpdated['data']));
     }
 
-    public function testCriteria(): void
+    public function testPutUnprocessable(): void
     {
-        $type = $this->createType();
-        $this->assertArrayHasKey('data', $type);
+        $query = $this->getDefault(['id' => '']);
 
-        $this->createBunch();
-        $this->assertEquals(Response::HTTP_CREATED, $this->client->getResponse()->getStatusCode());
-        $this->createBunchSecond();
-        $this->assertEquals(Response::HTTP_CREATED, $this->client->getResponse()->getStatusCode());
+        $this->put($query);
+        $this->assertEquals(Response::HTTP_UNPROCESSABLE_ENTITY, $this->client->getResponse()->getStatusCode());
 
-        $response = $this->criteria(["class" => static::getDtoClass(), "description" => "desc",]);
-        $this->assertEquals(Response::HTTP_OK, $this->client->getResponse()->getStatusCode());
-        $this->assertArrayHasKey('data', $response);
-        $this->assertCount(2, $response['data']);
+        $query = $this->getDefault(['bunch' => '']);
+
+        $this->put($query);
+        $this->assertEquals(Response::HTTP_UNPROCESSABLE_ENTITY, $this->client->getResponse()->getStatusCode());
+
+        $bunch       = BunchApiControllerTest::defaultData();
+        $bunch['id'] = '';
+        $query       = $this->getDefault(['bunch' => $bunch]);
+
+        $this->put($query);
+        $this->assertEquals(Response::HTTP_UNPROCESSABLE_ENTITY, $this->client->getResponse()->getStatusCode());
+
+        $query = $this->getDefault(['code' => '']);
+
+        $this->put($query);
+        $this->assertEquals(Response::HTTP_UNPROCESSABLE_ENTITY, $this->client->getResponse()->getStatusCode());
+
+        $code       = CodeApiControllerTest::defaultData();
+        $code['id'] = '';
+
+        $query = $this->getDefault(['code' => $code]);
+
+        $this->put($query);
+        $this->assertEquals(Response::HTTP_UNPROCESSABLE_ENTITY, $this->client->getResponse()->getStatusCode());
     }
 
     public function testPutNotFound(): void
     {
-        $this->put($this->getDefault(["description" => "0987654321",]));
+        $this->put($this->getDefault(["id" => 100, "description" => "0987654321",]));
         $this->assertEquals(Response::HTTP_NOT_FOUND, $this->client->getResponse()->getStatusCode());
     }
 
     public function testDeleteNotFound(): void
     {
-        $response = $this->delete('1');
+        $response = $this->delete(100);
+        $this->assertArrayHasKey('data', $response);
+        $this->assertEquals(Response::HTTP_NOT_FOUND, $this->client->getResponse()->getStatusCode());
+    }
+
+    public function testDelete(): void
+    {
+        $find = $this->assertGet(2);
+
+        $this->assertEquals(ActiveModel::ACTIVE, $find['data']['active']);
+
+        $this->delete(2);
+        $this->assertEquals(Response::HTTP_ACCEPTED, $this->client->getResponse()->getStatusCode());
+
+        $delete = $this->assertGet(2);
+
+        $this->assertEquals(ActiveModel::DELETED, $delete['data']['active']);
+    }
+
+    public function testPostUnprocessable(): void
+    {
+        $this->postWrong();
+        $this->assertEquals(Response::HTTP_UNPROCESSABLE_ENTITY, $this->client->getResponse()->getStatusCode());
+
+        $this->createConstraintBlankBunch();
+        $this->assertEquals(Response::HTTP_UNPROCESSABLE_ENTITY, $this->client->getResponse()->getStatusCode());
+
+        $this->createConstraintBlankCode();
+        $this->assertEquals(Response::HTTP_UNPROCESSABLE_ENTITY, $this->client->getResponse()->getStatusCode());
+    }
+
+    public function testPostDuplicate(): void
+    {
+        $this->createCodeDuplicate();
+        $this->assertEquals(Response::HTTP_CONFLICT, $this->client->getResponse()->getStatusCode());
+    }
+
+    public function testGetNotFound(): void
+    {
+        $response = $this->get(100);
         $this->assertArrayHasKey('data', $response);
         $this->assertEquals(Response::HTTP_NOT_FOUND, $this->client->getResponse()->getStatusCode());
     }
@@ -225,23 +198,54 @@ class BindApiControllerTest extends CaseTest implements ApiControllerTestInterfa
         $this->assertEquals(Response::HTTP_UNPROCESSABLE_ENTITY, $this->client->getResponse()->getStatusCode());
     }
 
-    public function testGetNotFound(): void
+    public function testGet(): void
     {
-        $response = $this->get(1);
-        $this->assertArrayHasKey('data', $response);
-        $this->assertEquals(Response::HTTP_NOT_FOUND, $this->client->getResponse()->getStatusCode());
-    }
+        $find = $this->assertGet(1);
 
-    public function testPostDuplicate(): void
-    {
-        $type = $this->createType();
-        $this->assertArrayHasKey('data', $type);
-
-        $this->createBunch();
-        $this->assertEquals(Response::HTTP_CREATED, $this->client->getResponse()->getStatusCode());
-
-        $this->createBunch();
-        $this->assertEquals(Response::HTTP_CONFLICT, $this->client->getResponse()->getStatusCode());
+        $this->assertStringContainsString(serialize($find['data']['bunch']['type']), serialize($find['data']['code']['type']));
     }
 //endregion Public
+
+//region SECTION: Private
+    private function assertGet(int $id): array
+    {
+        $find = $this->get($id);
+        $this->assertEquals(Response::HTTP_OK, $this->client->getResponse()->getStatusCode());
+
+        $this->checkResult($find);
+
+        return $find;
+    }
+
+    private function checkResult($entity):void
+    {
+        $this->assertArrayHasKey('data', $entity);
+        $this->assertArrayHasKey('bunch', $entity['data']);
+        $this->assertArrayHasKey('code', $entity['data']);
+        $this->assertArrayHasKey('type', $entity['data']['bunch']);
+        $this->assertArrayHasKey('type', $entity['data']['code']);
+    }
+
+    private function createConstraintBlankBunch(): array
+    {
+        $query = $this->getDefault(['bunch' => '']);
+
+        return $this->post($query);
+    }
+
+    private function createConstraintBlankCode(): array
+    {
+        $query = $this->getDefault(['code' => '']);
+
+        return $this->post($query);
+    }
+
+    private function createCodeDuplicate(): array
+    {
+        $query = $this->getDefault(['id' => 1]);
+
+        return $this->post($query);
+    }
+//endregion Private
+
 }
